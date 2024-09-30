be4da00b87dd349ac87cfb731eb97ecfcf33bc13
suleman111111111111111@gmail.com <suleman111111111111111@gmail.com>
2024-09-30 21:00:19 +0530

    1.2.023

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,10 +4,15 @@
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
+import Head from 'next/head';
 
 
 export const metadata = {
   title: "netz",
+  verification: {
+    google: 'WT9x6ycaN58WMURczi5-6Uk_pqt2_cvxkw2OIYN0ZPU',
+   
+    },
   description: "current objective :survive",
   icons:{
      icon:['/favicon.ico?v=4'],
@@ -20,7 +25,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    
+    <Head><meta name="google-site-verification" content="WT9x6ycaN58WMURczi5-6Uk_pqt2_cvxkw2OIYN0ZPU" /></Head>
       <body>
         <NavBar/>
       
