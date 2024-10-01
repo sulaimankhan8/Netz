20183b1495c4676c10cea26267fd1aeba4abaab1
suleman111111111111111@gmail.com <suleman111111111111111@gmail.com>
2024-10-02 00:16:47 +0530

    1.2.025

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+
+module.exports = {
+    siteUrl: 'https://netz-ruby.vercel.app', // Your domain
+    generateRobotsTxt: true, // (optional) Generate robots.txt
+    sitemapSize: 7000, // Optional limit of URLs per sitemap file
+    outDir: './public', // Specify the output directory
+  };
+  
