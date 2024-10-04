905ace551d92395f83c5d3050a6cbef8111e44bb
suleman111111111111111@gmail.com <suleman111111111111111@gmail.com>
2024-10-05 04:51:03 +0530

    1.2.037

--- src/app/algorithems/algorithems.gauss-seidale-method.js
+++ src/app/algorithems/algorithems.gauss-seidale-method.js
@@ -1,6 +1,11 @@
+'use client';
+
+import "katex/dist/katex.min.css";
+import { InlineMath, BlockMath } from "react-katex";
 import React, { useState } from 'react';
 import TButton from '../components/TButton';
 import Plot from '../Gauss-seidal/plot';
+import html2canvas from 'html2canvas';
 
 const GaussSeidel = () => {
   const [equations, setEquations] = useState([
@@ -11,8 +16,14 @@ const GaussSeidel = () => {
   const [demoInProgress, setDemoInProgress] = useState(false);
   const [error, setError] = useState(0.0001);
   const [results, setResults] = useState([]);
-  const [iterationDetails, setIterationDetails] = useState('');
+  
+  const [showMessage, setShowMessage] = useState(false);
+  const [iterationDetails, setIterationDetails] = useState([]);
+  const [iterationDetails2, setIterationDetails2] = useState([]);
+  const [iterationDetails3, setIterationDetails3] = useState([]);
   const [checkError, setCheckError] = useState('');
+
+
   const [variableSequence, setVariableSequence] = useState([]); // To track variable sequence
 
   // Handle input changes for equations
@@ -21,6 +32,48 @@ const GaussSeidel = () => {
     newEquations[index][field] = parseFloat(value);
     setEquations(newEquations);
   };
+  const handleInputChangeError = (e) => {
+    setError(e);
+  }
+
+  const exportGraphToPNG = () => {
+    const canvas = document.querySelector('canvas');
+    if (canvas) {
+      const link = document.createElement('a');
+      link.href = canvas.toDataURL("image/png");
+      link.download = 'graph.png';
+      link.click();
+    };
+    showCopied();
+  };
+
+  const exportTableToPNG = () => {
+    html2canvas(document.getElementById('diffTable')).then(canvas => {
+      const link = document.createElement('a');
+      link.href = canvas.toDataURL("image/png");
+      link.download = 'difference_table.png';
+      link.click();
+    });
+    showCopied();
+
+  };
+
+  const exportPolynomialStepsToPNG = () => {
+    html2canvas(document.getElementById('steps')).then(canvas => {
+      const link = document.createElement('a');
+      link.href = canvas.toDataURL("image/png");
+      link.download = 'polynomial_steps.png';
+      link.click();
+    }); showCopied();
+  };
+  
+  const showCopied = () => {
+    setShowMessage(true);
+
+    setTimeout(() => {
+      setShowMessage(false);
+    }, 2000);
+  }
 
   // Handle form submission
   const handleSubmit = (event) => {
@@ -38,20 +91,17 @@ const GaussSeidel = () => {
 
     setVariableSequence(sequence); // Update the variable sequence state
 
-    const { iterations, iterationsteps } = GaussSeidels(validatedEquations, error);
+    // Call GaussSeidels function
+    const { iterations, iterationsteps, iterationsteps2, iterationsteps3 } = GaussSeidels(validatedEquations, error);
 
+    // Update state with the results
     setResults(iterations);
-    setIterationDetails(iterationsteps);
+    setIterationDetails(iterationsteps); // Set the iteration steps directly
+    setIterationDetails2(iterationsteps2); // Set the second iteration steps directly
+    setIterationDetails3(iterationsteps3); // Set the third iteration steps directly
   };
 
-  // Generate HTML for iteration details
-  const generateIterationDetails = (iterations) => {
-    let details = '';
-    iterations.forEach(({ iteration, x, y, z }) => {
-      details += `Iteration ${iteration}: x = ${x.toFixed(4)}, y = ${y.toFixed(4)}, z = ${z.toFixed(4)}<br>`;
-    });
-    return details;
-  };
+
 
   // Handle Demo button click
   const handleDemo = async () => {
@@ -88,7 +138,7 @@ const GaussSeidel = () => {
   };
 
   return (
-    <div className="max-w-full mx-auto p-4">
+    <div className="max-w-full mx-auto p-4 dark:text-white dark:bg-neutral-700">
       <div className="flex justify-between items-center mb-6 text-slate-900 dark:text-white">
         <h1 className="text-2xl font-bold mb-4">Gauss-Seidel Method</h1>
 
@@ -107,11 +157,7 @@ const GaussSeidel = () => {
         </div>
       )}
 
-      {variableSequence.length > 0 && (
-        <div className="mb-4 p-4 bg-blue-100 text-blue-700 rounded">
-          <strong>Variable Sequence:</strong> {variableSequence.join(', ')}
-        </div>
-      )}
+
 
       <form onSubmit={handleSubmit} className="space-y-4">
         <table className="w-full m-auto table-auto md:table-fixed p-4 shadow-md">
@@ -133,9 +179,9 @@ const GaussSeidel = () => {
                     value={equation.a}
                     onChange={(e) => handleInputChange(index, 'a', e.target.value)}
                     required
-                    className="w-[50%] sm:my-5 sm:ml-5 text-black dark:bg-neutral-800 dark:text-white dark:border-gray-600 rounded-md hover:border hover:border-neutral-300 text-right"
+                    className="w-[50%] sm:my-5 sm:ml-5 text-black pr-1 dark:bg-neutral-800 dark:text-white border dark:border-gray-600 rounded-md hover:border hover:border-neutral-300 text-right"
                   />
-                  <label className="ml-0 sm:m-5 dark:text-white text-xl font-semibold">x</label>
+                  <label className="ml-0 md:m-5 dark:text-white text-xl font-semibold">x</label>
                 </td>
                 <td className="border border-gray-300 sm:p-2">
                   <input
@@ -144,9 +190,9 @@ const GaussSeidel = () => {
                     onChange={(e) => handleInputChange(index, 'b', e.target.value)}
                     placeholder="b"
                     required
-                    className="w-[50%] sm:my-5 sm:ml-5 text-black dark:bg-neutral-800 dark:text-white dark:border-gray-600 rounded-md hover:border hover:border-neutral-300 text-right"
+                    className="w-[50%] sm:my-5 sm:ml-5 text-black border pr-1 dark:bg-neutral-800 dark:text-white dark:border-gray-600 rounded-md hover:border hover:border-neutral-300 text-right"
                   />
-                  <label className="ml-0 sm:m-5 dark:text-white text-xl font-semibold">y</label>
+                  <label className="ml-0 md:m-5 dark:text-white text-xl font-semibold">y</label>
                 </td>
                 <td className="border border-gray-300 sm:p-2">
                   <input
@@ -155,12 +201,12 @@ const GaussSeidel = () => {
                     onChange={(e) => handleInputChange(index, 'c', e.target.value)}
                     placeholder="c"
                     required
-                    className="w-[50%] sm:my-5 sm:ml-5 text-black dark:bg-neutral-800 dark:text-white dark:border-gray-600 rounded-md hover:border hover:border-neutral-300 text-right"
+                    className="w-[50%] sm:my-5 sm:ml-5 text-black border pr-1 dark:bg-neutral-800 dark:text-white dark:border-gray-600 rounded-md hover:border hover:border-neutral-300 text-right"
                   />
-                  <label className="ml-0 sm:m-5 dark:text-white text-xl font-semibold">z</label>
+                  <label className="ml-0 md:m-5 dark:text-white text-xl font-semibold">z</label>
                 </td>
                 <td className="border border-gray-300 p-2">
-                  <label className="my-5 sm:ml-5 dark:text-white text-2xl font-bold">=</label>
+                  <label className="md:my-5 md:ml-5 dark:text-white text-2xl font-bold">=</label>
                   <input
                     type="number"
                     step="any"
@@ -168,13 +214,33 @@ const GaussSeidel = () => {
                     onChange={(e) => handleInputChange(index, 'constant', e.target.value)}
                     placeholder="Constant"
                     required
-                    className="w-[40%] p-2 text-black dark:bg-neutral-800 dark:text-white dark:border-gray-600 rounded-md hover:border hover:border-neutral-300"
+                    className="w-[40%] p-2 text-black dark:bg-neutral-800  dark:text-white border dark:border-gray-600 rounded-md hover:border hover:border-neutral-300"
                   />
                 </td>
               </tr>
+
             ))}
+            <tr>
+              <td className="border border-gray-300 p-5 text-xl text-center" colSpan="2">Error Margin</td>
+              <td className=" border border-gray-300 p-2" colSpan="2">
+                <input
+                  type="number"
+                  step="any"
+                  value={error}
+                  onChange={(e) => handleInputChangeError(e.target.value)}
+                  placeholder="0.0001"
+                  required
+                  className="w-[80%] p-2 text-black m-auto dark:bg-neutral-800 dark:text-white border dark:border-gray-600 rounded-md pr-1 hover:border hover:border-neutral-300"
+                />
+              </td>
+
+            </tr>
           </tbody>
         </table>
+
+
+
+
 
         <div className="flex justify-between">
           <button
@@ -197,20 +263,102 @@ const GaussSeidel = () => {
         </div>
       </form>
 
-      <div className="mb-6">
-        <h2 className="text-xl font-semibold mb-2">Results:</h2>
-        <div
-          className="border p-4 rounded"
-          dangerouslySetInnerHTML={{ __html: iterationDetails }}
-        />
-      </div>
+
+      {variableSequence.length > 0 && (
+        <div className="my-4 p-4 bg-blue-100 text-blue-700 rounded">
+          <strong>Variable Sequence:</strong> {variableSequence.join(', ')}
+        </div>
+      )}
 
       {results.length > 0 && (
-        <div className="mb-6">
-          <h2 className="text-xl font-semibold mb-2">Chart:</h2>
+        <div className="mt-6 mx-auto dark:bg-neutral-600 p-8 rounded-2xl  hover:border hover:border-neutral-300">
+          <TButton onClick={exportGraphToPNG} tooltipText="Export&nbsp;Graph&nbsp;to&nbsp;PNG"
+          color="blue"
+          
+           altText="Export Graph" 
+           imgSrc="/copy.svg" 
+           float="float-right" />
+          <h2 className="text-xl font-semibold mb-2">Plot:</h2>
           <Plot iterations={results} />
         </div>
       )}
+
+<div id="messageBox" style={{
+        display: showMessage ? 'block' : 'none',
+        backgroundColor: '#4CAF50',
+        color: 'white',
+        padding: '10px',
+        position: 'fixed',
+        bottom: '30px',
+        right: '20px',
+        borderRadius: '5px',
+      }}>
+        copied
+      </div>
+      {results.length > 0 && (<div>
+        
+
+
+        <div id="diffTable"  className="my-[3rem]  md:mx-[16rem] overflow-x-auto dark:bg-neutral-700">
+          <h2 className="text-xl inline-block font-semibold">Iteration Results:</h2><TButton onClick={exportTableToPNG} tooltipText="Export&nbsp;Table to&nbsp;PNG" color="blue" className="overflow-visible " altText="Export Table" imgSrc="/copy.svg" float="float-right" />
+          <table className="w-full table-auto">
+            <thead>
+              <tr>
+                <th className="border border-gray-300 p-2">Iteration</th>
+                <th className="border border-gray-300 p-2">x</th>
+                <th className="border border-gray-300 p-2">y</th>
+                <th className="border border-gray-300 p-2">z</th>
+              </tr>
+            </thead>
+            <tbody>
+              {results.map((result, index) => (
+                <tr key={index} className={index === results.length-1 ? 'bg-red-700':''}>
+                  <td className="border border-gray-300 p-2">{result.iteration}</td>
+                  <td className="border border-gray-300 p-2">{result.x.toFixed(4)}</td>
+                  <td className="border border-gray-300 p-2">{result.y.toFixed(4)}</td>
+                  <td className="border border-gray-300 p-2">{result.z.toFixed(4)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        </div>)}
+        <div id="steps" className="mt-6 text-wrap dark:bg-neutral-700">
+      
+        <h2 className="text-xl font-semibold mb-2">Results:</h2>
+        <TButton onClick={exportPolynomialStepsToPNG} tooltipText="Export&nbsp;Polynomial Steps&nbsp;to&nbsp;PNG" color="blue" altText="Export&nbsp;steps" className="" float="float-right" imgSrc="/copy.svg" />
+        <div className="border p-4 rounded">
+          {iterationDetails.length && (
+            iterationDetails.map((step, index) => (
+              <div key={index} className="">
+                <h2 className="text-xl md:ml-10 overflow-x-auto">Iteration {`${index + 1}`}</h2><br />
+
+                <div className="text-center rounded-lg">
+                  <pre className="overflow-x-auto">
+                <BlockMath math={`${step}  \\newline`} />
+                  </pre>
+                </div>
+                <div className="text-center  rounded-lg">
+                  <pre className="overflow-x-auto">
+                  <BlockMath math={`${iterationDetails2[index]} `} />
+                  </pre>
+                </div>
+                <div className="text-center  rounded-lg">
+                  <pre className="overflow-x-auto">
+                  <BlockMath math={`${iterationDetails3[index]}`} />
+                  </pre>
+                </div>
+                  
+
+                <br />
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+
+
+
     </div>
   );
 };
@@ -301,14 +449,18 @@ function GaussSeidels(equations, error) {
     yPrev = 0,
     zPrev = 0;
   let iterations = [];
-  let iterationsteps = '';
+  let iterationsteps = [];
+  let iterationsteps2 = [];
+  let iterationsteps3 = [];
   let iterationCount = 1;
 
   // Initialize with iteration 0
   iterations.push({ iteration: 0, x: xPrev, y: yPrev, z: zPrev });
 
   while (true) {
-    let detailedIterations = `Iteration ${iterationCount}:<br>`;
+    let detailedIterations = ` `;
+    let detailedIterations2 = ` `;
+    let detailedIterations3 = ` `;
 
     // To store new values temporarily
     let newValues = {};
@@ -321,19 +473,29 @@ function GaussSeidels(equations, error) {
       if (solveFor === 'x') {
         newValue = (constant - b * yPrev - c * zPrev) / a;
         newValues.x = newValue;
-        detailedIterations += `x<sup>${iterationCount}</sup> = (${constant} - (${b}) * y<sup>${iterationCount - 1}</sup> - (${c}) * z<sup>${iterationCount - 1}</sup>) / ${a} = ${newValue.toFixed(4)}<br>`;
+        detailedIterations += `\\displaystyle x^{(${iterationCount})} = 
+        \\frac{${constant} - (${b}) \\cdot y^{(${iterationCount - 1})} - (${c}) \\cdot z^{(${iterationCount - 1})}}{${a}} `
+        detailedIterations += `\\displaystyle =
+        \\frac{${constant} - (${a}) \\cdot (${xPrev.toFixed(4)}) - (${b}) \\cdot (${yPrev.toFixed(4)})}{${a}}  `
+        detailedIterations += `\\displaystyle  =${newValue.toFixed(4)} \\quad `;
       } else if (solveFor === 'y') {
         newValue = (constant - a * newValues.x - c * zPrev) / b;
         newValues.y = newValue;
-        detailedIterations += `y<sup>${iterationCount}</sup> = (${constant} - (${a}) * x<sup>${iterationCount}</sup> - (${c}) * z<sup>${iterationCount - 1}</sup>) / ${b} = ${newValue.toFixed(4)}<br>`;
+        detailedIterations2 += `\\displaystyle y^{(${iterationCount})} = \\frac{${constant} - (${a}) \\cdot x^{(${iterationCount})} - (${c}) \\cdot z^{(${iterationCount - 1})}}{${b}} `
+        detailedIterations2 += `\\displaystyle  = \\frac{(${constant} - ${a} \\cdot (${newValues.x.toFixed(4)}) - ${c} \\cdot (${zPrev.toFixed(4)}))}{(${b})} `
+        detailedIterations2 += `\\displaystyle = ${newValue.toFixed(4)} \\quad `;
       } else if (solveFor === 'z') {
         newValue = (constant - a * newValues.x - b * newValues.y) / c;
         newValues.z = newValue;
-        detailedIterations += `z<sup>${iterationCount}</sup> = (${constant} - (${a}) * x<sup>${iterationCount}</sup> - (${b}) * y<sup>${iterationCount}</sup>) / ${c} = ${newValue.toFixed(4)}<br>`;
+        detailedIterations3 += `\\displaystyle z^{(${iterationCount})} = \\frac{${constant} - (${a}) \\cdot x^{(${iterationCount})} - (${b}) \\cdot y^{(${iterationCount})}}{${c}} `
+        detailedIterations3 += `\\displaystyle  =  \\frac{(${constant} - ${a} \\cdot (${newValues.x.toFixed(4)}) - ${c} \\cdot (${zPrev.toFixed(4)}))}{(${b})}  `
+        detailedIterations3 += `\\displaystyle = ${newValue.toFixed(4)} \\quad `;
       }
     });
+    iterationsteps.push(detailedIterations);
+    iterationsteps2.push(detailedIterations2);
+    iterationsteps3.push(detailedIterations3);
 
-    iterationsteps += detailedIterations + '<br>';
 
     // Update iteration values
     const xNew = newValues.x !== undefined ? newValues.x : xPrev;
@@ -364,7 +526,7 @@ function GaussSeidels(equations, error) {
     }
   }
 
-  return { iterations, iterationsteps };
+  return { iterations, iterationsteps, iterationsteps2, iterationsteps3, };
 }
 
 export default GaussSeidel;
