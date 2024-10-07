f25e835eda71b896b1a6819776907133c72b3fde
suleman111111111111111@gmail.com <suleman111111111111111@gmail.com>
2024-10-08 00:36:07 +0530

    1.2.047

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,22 +1,218 @@
 export default function sitemap() {
-    return [
-      {
-        url: 'https://netz-ruby.vercel.app',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 1,
-      },
-      {
-        url: 'https://netz-ruby.vercel.app/newton-backward',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://netz-ruby.vercel.app/newton-foward',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.5,
-      },
-    ]
-  }
+  return [
+    {
+      url: 'https://netz-ruby.vercel.app/',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'daily',
+      priority: 1.0,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/Algorithems',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/Home',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/Notes',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/Playground',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/Profile',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/Setting',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+
+    // UNIT 1
+    {
+      url: 'https://netz-ruby.vercel.app/bisection-method',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/false-position-method',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/Gauss-seidal',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/iteration-method',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/newton-raphson-method',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+
+    // UNIT 2
+    {
+      url: 'https://netz-ruby.vercel.app/gauss-backward',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/gauss-forward',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/newton-backward',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/newton-forward',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/lagrange-interpolation',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/newton-divided',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+
+    // UNIT 3
+    {
+      url: 'https://netz-ruby.vercel.app/boole-Rule',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/simpson-1-3-Rule',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/simpson-3-8-Rule',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/trapezoidal-Rule',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/weddle-Rule',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+
+    // UNIT 4
+    {
+      url: 'https://netz-ruby.vercel.app/euler-s-method',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/modified-euler-s-method',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/runge-kutta-method',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/taylor-s-series-method',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+
+    // UNIT 5
+    {
+      url: 'https://netz-ruby.vercel.app/chi-square',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/f-test',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/t-test',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/test-significance',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/fitting-parabola',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/fitting-straight-lines',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://netz-ruby.vercel.app/least-squares',
+      lastModified: new Date('2024-10-01T00:00:00+00:00'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+  ];
+}
