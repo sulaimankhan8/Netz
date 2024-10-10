9e47036c34e11c01cf32cf23bb82dbed90799fae
suleman111111111111111@gmail.com <suleman111111111111111@gmail.com>
2024-10-10 15:34:13 +0530

    1.2.085

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import '../styles/globals.css';
-import '../app/styles/globals.css';
+import './styles/globals.css';
 import Head from 'next/head';
 import Layout from '../app/components/layout'; // Ensure the spelling is correct (Layout vs. Laout)
 
